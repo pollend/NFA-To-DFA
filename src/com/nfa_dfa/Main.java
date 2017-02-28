48698463d633e4ee52268e4d64b720d17cdb2fb5
Chris Elliott <ChrisElliott@Chris-MacBook-Pro-4.local>
2017-02-28 11:59:29 -0800

    updated start state

--- src/com/nfa_dfa/Main.java
+++ src/com/nfa_dfa/Main.java
@@ -1,5 +1,3 @@
-package com.nfa_dfa;
-
 
 import javax.swing.plaf.nimbus.State;
 import java.io.File;
@@ -12,27 +10,31 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
-
+    
     public static void main(String[] args) throws FileNotFoundException {
-        Parser p = new Parser("test.nfa");
-
+        String firstArg =" ";
+        if(args.length > 0);{
+            firstArg = args[0];
+        }
+        
+        Parser p = new Parser(firstArg);
         Queue<String[]> StateToTest = new LinkedList<>();
-
+        
         HashSet<String[]> state = new HashSet<>();
-        HashSet<String> transitions = new HashSet<>(); 
-
-
-        //add starting state to list
+        HashSet<String> transitions = new HashSet<>();
+        
+        
+        //add starting state to queue
         StateToTest.add(new String[]{p.StartingState()});
-         //do conversion
+        //do conversion
         while (StateToTest.size() > 0) {
             String[] test = StateToTest.remove();
-
+            
             for (String c : p.EpsilonClosure()) {
                 HashSet<String> visits = new HashSet<>();
                 for (int x = 0; x < test.length; x++) {
                     Main.Visit(p, test[x], c, visits, true);
-
+                    
                 }
                 String key = ConvertToHashKey(Arrays.stream(test), c, visits.stream());
                 if (!transitions.contains(key)) {
@@ -41,29 +43,29 @@ public class Main {
                 }
                 state.add(test);
                 state.add(visits.toArray(new String[visits.size()]));
-
-
+                
             }
         }
-
-
-        //create file 
-        File f2  = new File("test.dfa");
+        
+        
+        //create file
+        File f2  = new File("DFA.dfa");
         try {
             f2.createNewFile();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+        
         try {
+            int count = -1;
             FileWriter writer = new FileWriter(f2);
-
-
+            
+            
             writer.write(state.stream().map(j -> GroupTokens(Arrays.stream(j))).distinct().collect(Collectors.joining("\t")) + "\n");     //write states
-            writer.write(Arrays.stream(p.EpsilonClosure()).collect(Collectors.joining("\t")) + "\n");  //write epsilon closures 
-            writer.write("{"+p.StartingState() + "}\n"); //write start state
+            writer.write(Arrays.stream(p.EpsilonClosure()).collect(Collectors.joining("\t")) + "\n");  //write sigma
+            writer.write("{" + p.StartingState()+ "," + p.DFAState()[++count] + "} \n"); //write start state
             writer.write(state.stream().filter(j -> Arrays.stream(j).anyMatch(r -> Arrays.asList(p.AcceptedStates()).contains(r))).map(x -> GroupTokens(Arrays.stream(x))).distinct().collect(Collectors.joining("\t")) + "\n");  //write Accept States
-
+            
             for (String transition:transitions) {   //write transitions
                 writer.write(transition + "\n");
             }
@@ -71,11 +73,11 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
-
+        
+        
+        
     }
-    //this method finds duplicates and then adds it to result
+    //this method finds matches and then adds it to result
     public  static List<String[]> FindMatching(HashSet<String[]> states,String matchToken)
     {
         List<String[]> result = new ArrayList<>();
@@ -88,36 +90,41 @@ public class Main {
                     break;
                 }
             }
-
+            
         }
         return result;
     }
-
-
+    
+    
     public static String GroupTokens(Stream<String> str) {
         return "{" + str.collect(Collectors.joining(", ")) + "}";
     }
-
+    
     public static String ConvertToHashKey(Stream<String> states, String key, Stream<String> to) {
         return GroupTokens(states) + "," + key + "=" + GroupTokens(to);
     }
-    //keep track of what is visited 
+    //keep track of what is visited
     public static void Visit(Parser p, String state, String symbol, HashSet<String> visit, boolean start) {
         if (!start) {
             if (visit.contains(state))
                 return;
             visit.add(state);
         }
-
+        
         Iterator<String> iter2 = p.GetTransitions(state, "EPS");
+        int count= 0;
         if (iter2 != null) {
             for (Iterator<String> it = iter2; it.hasNext(); ) {
                 String nextState = it.next();
+                p.DFAState()[count] = nextState;
+                
                 Main.Visit(p, nextState, symbol, visit, false);
+                count++;
             }
+            
         }
-
-
+        
+        
         Iterator<String> iter = p.GetTransitions(state, symbol);
         if (iter != null) {
             for (Iterator<String> it = iter; it.hasNext(); ) {
@@ -125,8 +132,8 @@ public class Main {
                 Main.Visit(p, nextState, symbol, visit, false);
             }
         }
-
+        
     }
-
-
+    
+    
 }
--- src/com/nfa_dfa/Parser.java
+++ src/com/nfa_dfa/Parser.java
@@ -1,4 +1,4 @@
-package com.nfa_dfa;
+
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -7,32 +7,33 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Parser {
-
-
+    
+    
     private String startingState;
     private String[] possibleStates;
     private String[] acceptedStates;
     private String[] epsilonClosure;
+    private String[] dfaState = new String[10];
     private HashMap<String, HashMap<String,List<String>>> transition = new HashMap<>();
-
+    
     public  Parser(String path) throws FileNotFoundException {
         File file = new File(path);
         Scanner scanner = new Scanner(file);
-
+        
         if(!scanner.hasNext()) throw new RuntimeException("illegal file");
         possibleStates = scanner.nextLine().trim().split("\\s+");
-
+        
         if(!scanner.hasNext()) throw new RuntimeException("illegal file");
         epsilonClosure = scanner.nextLine().trim().split("\\s+");
-
+        
         if(!scanner.hasNext()) throw new RuntimeException("illegal file");
         startingState =  scanner.nextLine().trim();
-
+        
         if(!scanner.hasNext()) throw new RuntimeException("illegal file");
         acceptedStates = scanner.nextLine().trim().split("\\s+");
-
-        Pattern p = Pattern.compile("([a-zA-Z0-9]+),([a-zA-Z0-9]+)=([a-zA-Z0-9]+)");
-
+        
+        Pattern p = Pattern.compile("([a-zA-Z0-9{,}]+),([a-zA-Z0-9]+)=([a-zA-Z0-9{,}]+)");
+        
         for (String state : possibleStates) {
             transition.put(state,new HashMap<String, List<String>>());
         }
@@ -40,7 +41,7 @@ public class Parser {
         {
             String line = scanner.nextLine().replaceAll("\\s","").trim();
             System.out.println(line);
-
+            
             Matcher match = p.matcher(line);
             match.find();
             if(!match.hitEnd() && match.groupCount() != 3)
@@ -48,51 +49,54 @@ public class Parser {
             String currentState = match.group(1);
             String symbol = match.group(2);
             String next = match.group(3);
-
+            
             Map<String,List<String>> lookup = transition.get(currentState);
             if(lookup == null) {
                 transition.put(currentState, new HashMap<>());
                 lookup = transition.get(currentState);
             }
-
+            
             List<String> tr = null;
             if(!lookup.containsKey(symbol)) {
                 lookup.put(symbol, new ArrayList<String>());
                 tr = lookup.get(symbol);
             }
             else tr = lookup.get(symbol);
-
+            
             tr.add(next);
-
+            
         }
     }
-
-
+    
+    
     public Iterator<String> GetTransitions(String state,String symbol)
     {
         if(!transition.get(state).containsKey(symbol))
             return  null;
-
+        
         return transition.get(state).get(symbol).iterator();
     }
-
+    
     public  String StartingState()
     {
         return  startingState;
     }
-
+    
     public  String[] PossibleStates(){
         return possibleStates;
     }
-
+    
     public  String[] AcceptedStates(){
         return acceptedStates;
     }
-
+    
     public String[] EpsilonClosure(){
         return  epsilonClosure;
     }
-
-
-
+    
+    public String[] DFAState(){
+        return dfaState;
+    }
+    
+    
 }
